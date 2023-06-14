81782d76629369a9beb8a6f1422d86dd9657a969
Marty Oehme <marty.oehme@gmail.com>
2023-06-14 11:47:17 +0200

    Load astro site option from env file

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,13 @@
 import { defineConfig } from "astro/config";
+import { loadEnv} from "vite";
 
 import tailwind from "@astrojs/tailwind";
 
+const { SITE } = loadEnv(import.meta.env.MODE, process.cwd(), "");
+let site = SITE ?? "https://martyoeh.me"
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+    site: site,
+    integrations: [tailwind()],
 });
