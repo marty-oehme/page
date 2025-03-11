ed605faeb23de211af92f19637407ed88cb74eb5
Marty Oehme <contact@martyoeh.me>
2025-03-11 21:37:12 +0100

    components: Add icons to Aside component

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,24 +4,35 @@ import tailwind from "@astrojs/tailwind";
 import remarkToc from "remark-toc";
 import { remarkReadingTime } from "./src/plugins/remark/reading-time.mjs";
 import icon from "astro-icon";
-const {
-  SITE
-} = loadEnv(import.meta.env.MODE, process.cwd(), "");
-let site = SITE ?? "https://martyoeh.me";
+import mdx from "@astrojs/mdx";
 
+const { SITE } = loadEnv(import.meta.env.MODE, process.cwd(), "");
+let site = SITE ?? "https://martyoeh.me";
 
 // https://astro.build/config
 export default defineConfig({
   site: site,
-  integrations: [tailwind({
-    config: {
-      applyBaseStyles: false
-    }
-  }), icon()],
+  experimental: { contentLayer: true },
+  integrations: [
+    tailwind({
+      config: {
+        applyBaseStyles: false,
+      },
+    }),
+    icon(),
+    mdx(),
+  ],
   markdown: {
-    remarkPlugins: [remarkReadingTime, [remarkToc, {
-      tight: true,
-      depth: 3
-    }]]
-  }
+    remarkPlugins: [
+      remarkReadingTime,
+      [
+        remarkToc,
+        {
+          tight: true,
+          depth: 3,
+        },
+      ],
+    ],
+  },
 });
+
