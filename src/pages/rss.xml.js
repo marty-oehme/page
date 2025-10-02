d941e30722954037f4a53e25987cdb6427dfdf83
Marty Oehme <contact@martyoeh.me>
2025-10-02 17:28:13 +0200

    feat(rss): Improve RSS feed
    Feed got a very pretty style
    (from https://github.com/genmon/aboutfeeds/blob/main/tools/pretty-feed-v3.xsl).
    
    It is reverse date ordered, and slices to only display the last 50 posts.
    Trailing slashes are removed from entries to have the same url as posts
    themselves.
    
    Fulltext is kept in the rss file - though I have to check how well it
    works for mdx files and more complicated posts.
    The content is parsed with markdown-it and sanitized with sanitize-html.
    
    And it is made automatically discoverable on all pages by embedding into
    the head of the base layout, so a feed reader should just automatically
    find the rss file when pointed at any page.

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -2,18 +2,29 @@ import rss from "@astrojs/rss";
 import { getCollection } from "astro:content";
 import { BASE_URL } from "../data/constants";
 
+import sanitizeHtml from "sanitize-html";
+import MarkdownIt from "markdown-it";
+const parser = new MarkdownIt();
+
 export async function GET() {
   const entries = await getCollection("blog");
   return rss({
+    stylesheet: "/rss/style.xsl",
     title: "Marty's blog",
     description: "Deep dives into uncertainty",
     site: BASE_URL,
-    items: entries.map((post) => ({
-      title: post.data.title,
-      description: post.data.description,
-      pubDate: post.data.pubDate,
-      link: `/blog/${post.id}`,
-    })),
+    trailingSlash: false,
+    items: entries
+      .map((post) => ({
+        title: post.data.title,
+        description: post.data.description,
+        pubDate: post.data.pubDate,
+        link: `/blog/${post.id}`,
+        content: sanitizeHtml(parser.render(post.body), {
+          allowedTags: sanitizeHtml.defaults.allowedTags.concat(["img"]),
+        }),
+      }))
+      .sort((a, b) => b.pubDate - a.pubDate).slice(0,50),
     customData: `<language>en-us</language>`,
   });
 }
