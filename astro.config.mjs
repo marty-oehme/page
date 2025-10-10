3f530729a8f03cdb5c163bfe433684709a44100f
Marty Oehme <contact@martyoeh.me>
2025-10-10 14:51:15 +0200

    feat(repo): Switch code display to expressive code
    Remove the manually added shiki transformers and simply make use of the
    existing expressive code style astro plugin. Can take care of rendering
    not just diffs (what I used transformers before previously), but file
    names, word wraps, highlights, descriptions, line numbers and more.

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,8 @@ import remarkToc from "remark-toc";
 import { remarkReadingTime } from "./src/plugins/remark/reading-time.mjs";
 import icon from "astro-icon";
 import mdx from "@astrojs/mdx";
-import { transformerNotationDiff } from "@shikijs/transformers";
+
+import expressiveCode from "astro-expressive-code";
 
 const { SITE } = loadEnv(import.meta.env.MODE, process.cwd(), "");
 let site = SITE ?? "https://martyoeh.me";
@@ -13,19 +14,12 @@ let site = SITE ?? "https://martyoeh.me";
 // https://astro.build/config
 export default defineConfig({
   site: site,
-  integrations: [
-    tailwind({
-      config: {
-        applyBaseStyles: false,
-      },
-    }),
-    icon(),
-    mdx(),
-  ],
-  markdown: {
-    shikiConfig: {
-      transformers: [transformerNotationDiff()],
+  integrations: [tailwind({
+    config: {
+      applyBaseStyles: false,
     },
+  }), icon(), expressiveCode(), mdx()],
+  markdown: {
     remarkPlugins: [
       remarkReadingTime,
       [
